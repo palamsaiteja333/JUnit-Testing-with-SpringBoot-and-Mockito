beb730ec0b3be02b3d3f61854f9aac60095bc411
palamsaiteja333 <palamsaiteja333@gmail.com>
2023-06-26 09:44:39 -0300

    unit-testing

---
+++ src/main/java/com/learning/unittesting/unittesting/businesslayer/CalculateSum.java
@@ -0,0 +1,30 @@
+package com.learning.unittesting.unittesting.businesslayer;
+
+import com.learning.unittesting.unittesting.datalayer.DataCalculate;
+
+public class CalculateSum {
+	
+	private DataCalculate datacalculate;
+	
+	public int sumCalculation(int[] data) {
+		int sum=0;
+		
+		for(int value:data) {
+			sum= sum +value;
+		}
+		
+		return sum;
+	}
+	
+	public int sumCalculationUsingDataLayer() {
+		int sum=0;
+		int[] data = datacalculate.retrieveData();
+		
+		for(int value:data) {
+			sum= sum +value;
+		}
+		
+		return sum;
+	}
+
+}
---
+++ src/test/java/com/learning/unittesting/unittesting/businesslayer/CalculateSumTestClass.java
@@ -0,0 +1,39 @@
+package com.learning.unittesting.unittesting.businesslayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateSumTestClass {
+
+	@Test
+	public void sumCalculationBasicCase() {
+		
+		CalculateSum sum = new CalculateSum();
+		int actualResult = sum.sumCalculation(new int[] {1, 2, 3});
+		int expectedResult = 6;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void sumCalculationNullCase() {
+		
+		CalculateSum sum = new CalculateSum();
+		int actualResult = sum.sumCalculation(new int[] {});
+		int expectedResult = 0;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void sumCalculationSingleValue() {
+		
+		CalculateSum sum = new CalculateSum();
+		int actualResult = sum.sumCalculation(new int[] {1});
+		int expectedResult = 1;
+		
+		assertEquals(expectedResult, actualResult);
+	}
+
+}
